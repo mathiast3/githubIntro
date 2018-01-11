44be17299c00f54778c3c78501774e4d1a919732
Mathias Taylor <mtaylor53@mail.greenriver.edu>
2018-01-11 17:11:34 -0500

    message

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
+<?php
+
+require "vendor/autoload.php";
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define default route
+$f3->route('GET /', function(){
+    echo '<h1>Hello, World</h1>';
+});
+
+//run fat-free
+$f3->run;
+?>
 <h1>Hi</h1>
+<h2>Hello, world</h2>
